a04c94649caf6e18f36eb6ac63aa181d1b247f22
Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2021-04-09 17:38:54 +0100

    Update filter_rscc.py

--- Data Splitting/filter_rscc.py
+++ Data Splitting/filter_rscc.py
@@ -1,9 +1,20 @@
 import os
 
-file_name = 'myData.txt'
+file_name = 'filtered_PDB_RSCC.txt'
 
-sugar = ""
+filterFile = None
 
 with open(file_name) as base_file:
     for lineNo, line in enumerate(base_file):
-        print(line)
+        if lineNo != 0:
+            print(lineNo)
+            temp = line.split(',')
+            if temp[6] == "":
+                continue
+            if float(temp[6]) > 0.8:
+                if filterFile is not None:
+                    filterFile.close()
+                filterFile = open("filtered_PDB_RSCC_0.8.txt",'a')
+                filterFile.write(line)
+    if filterFile:
+        filterFile.close()
