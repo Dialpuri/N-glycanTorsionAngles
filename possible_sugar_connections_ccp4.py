166aab93b4fa4a3c14f3ff67d8310504f2e21762
Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2021-04-09 16:43:53 +0100

    Initial Commit

---
+++ possible_sugar_connections_ccp4.py
@@ -0,0 +1,27 @@
+import os
+
+file_name = raw_input("What is the name of the text file (enter in format; NAME.txt) ")
+
+sugar_name = ""
+sugar_chain = ""
+sugar_number = ""
+pdb_code = ""
+
+with open(file_name, "r") as myFile:
+    for lineNo, line in enumerate(myFile):
+        if lineNo != 0:
+            split_line = line.split(',')
+            if split_line[0] == pdb_code:
+                sugar_chain_position = split_line[1].split('-')
+                sugar_chain_position[2].replace(" ", "")
+
+               # if sugar_chain_position[0] == sugar_name:
+                if sugar_chain_position[1] == sugar_chain:
+                    if int(sugar_chain_position[2]) == (int(sugar_number) + 1):
+                        print("Potential Joined Chains found \n PDB CODE: {}, Sugars: {} and {}, Chain: {}, Numbers:{} and {}".format(pdb_code, sugar_name, sugar_chain_position[0], sugar_chain_position[1], sugar_number, sugar_chain_position[2]))
+
+                sugar_name = sugar_chain_position[0]
+                sugar_chain = sugar_chain_position[1]
+                sugar_number = sugar_chain_position[2].strip()
+            
+            pdb_code = split_line[0] 
